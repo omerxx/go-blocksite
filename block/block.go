dff50e689748795fcb32fc0f3b1eb9e486108b12
Omer Hamerman <omerxx@gmail.com>
2021-06-17 17:45:30 +0300

    WIP live targets

--- block/block.go
+++ block/block.go
@@ -2,23 +2,47 @@ package block
 
 import (
   "fmt"
+  "net/http"
 	"github.com/omerxx/go-blocksite/state"
 	"github.com/spf13/viper"
 	"github.com/txn2/txeh"
 )
 
-func BlockSites(hosts *txeh.Hosts, sites []string) {
+func isTargetAlive(url string) bool {
+  resp, err := http.Get(fmt.Sprintf("https://%s", url))
+  if err != nil {
+    fmt.Printf("Can't probe: %s\n", url)
+    // TODO switch back --->
+    return true
+    // return false
+  }
+  if resp.StatusCode >= 200 && resp.StatusCode < 500 {
+    return true
+  } else {
+    fmt.Printf("Status code for %s is %d, Ignoring\n", url, resp.StatusCode)
+    return false
+  }
+}
+
+func BlockSites(hosts *txeh.Hosts, sites []string) (blacklistConfiguredSites []string){
+  for _, s := range sites {
+    blacklistConfiguredSites = append(blacklistConfiguredSites, s)
+  }
+  sites = state.FilterStateListedSites(sites)
+
 	for _, site := range sites {
-		blockSite(hosts, site)
+    if isTargetAlive(site) {
+      blockSite(hosts, site)
+    } else {
+      blacklistConfiguredSites, _ = state.RemoveUrlFromList(blacklistConfiguredSites, site)
+    }
 	}
+  return blacklistConfiguredSites
 }
 
 func blockSite(hosts *txeh.Hosts, site string) {
-	target := viper.GetString("app.blockTarget")
-
+	target := viper.GetString("app.blockRoute")
 	hosts.AddHost(target, site)
-  wsite := fmt.Sprintf("www.%s", site)
-	hosts.AddHost(target, wsite)
 	hosts.Save()
 }
 
